9c4122ad175524f81e71d4132f8ed9348fbb55a6
Priyabrata9861 <ptiyabratamishra2001@gmail.com>
2025-05-25 12:30:12 +0530

    chanes

--- FitPeoAssignment/vite.config.js
+++ FitPeoAssignment/vite.config.js
@@ -3,10 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  //  base: process.env.VITE_BASE_PATH || '/fit-peo-assignment',
   base: './',
   plugins: [react()],
-  optimizeDeps: {
-    include: ['react-apexcharts', 'apexcharts']
-  }
 })
